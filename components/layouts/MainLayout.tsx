c3b3b4790b4307315c28033ac2cd9288c2c4bc3a
Santiago Reynoso <snreynoso@gmail.com>
2022-09-27 20:51:26 -0300

    first commit

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,9 +1,15 @@
-import Head from 'next/head';
-import { Navbar } from '../Navbar';
+import React, { FC } from 'react';
 
+import Head from 'next/head';
+
+import { Navbar } from '../Navbar';
 import styles from './MainLayout.module.css';
 
-export const MainLayout = ({ children }) => {
+type Props = {
+    children?: React.ReactNode;
+}
+
+export const MainLayout: React.FC<Props> = ({ children }) => {
     return (
         <div className={styles.container}>
             <Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { MainLayout } from '../components/layouts/MainLayout'
 export default function HomePage() {
     return (
         <MainLayout>
+            
             <h1>Home Page!</h1>
 
             <h1 className={'title'}>
